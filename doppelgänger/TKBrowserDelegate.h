286b7fe993e9aac58f7efbde41390d09b1ec35e5
Tommy Knowlton <tknowlton@iamhisfriend.org>
2012-06-11 01:21:57 -0600

    Committing first milestone - display mounted volumes in first column.

---
+++ doppelgänger/TKBrowserDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TKBrowserDelegate.h
+//  doppelgänger
+//
+//  Created by Tommy Knowlton on 6/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKBrowserDelegate : NSObject
+
+@end
---
+++ doppelgänger/TKBrowserDelegate.m
@@ -0,0 +1,13 @@
+//
+//  TKBrowserDelegate.m
+//  doppelgänger
+//
+//  Created by Tommy Knowlton on 6/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TKBrowserDelegate.h"
+
+@implementation TKBrowserDelegate
+
+@end
